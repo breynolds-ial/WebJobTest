eb2650d5787bd0d6818db728bb046f3515b1e63c
Brian <brian.reynolds@inchcape.com.au>
2018-08-01 09:36:08 +1000

    First commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebJobTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine($"Hello World! It is {DateTime.Now}");
+                System.Threading.Thread.Sleep(10000);
+            }
+        }
+    }
+}
